601c13bb0a10d7ef184875f7ca2f37c53fc8e1c7
wangzitao <417301709@qq.com>
2019-05-16 15:18:00 +0800

    delete the .idea

---
+++ Utils/src/main/java/com/hfut/utils/model/User.kt
@@ -0,0 +1,16 @@
+package com.hfut.base.model
+
+import com.hfut.base.utils.GsonUtils
+
+/**
+ * Created by wzt on 2019/5/16
+ * 用户类
+ */
+data class User(var id: Long, var createDate: Long, var name: String, var hxAccount: String?, var hxPassword: String?, var phone: String?, var age: Int, var gender: Int) {
+    companion object {
+        @JvmStatic
+        fun fromJson(json: String): User? {
+            return GsonUtils.fromJson(json, User::class.java)
+        }
+    }
+}
---
+++ Utils/src/main/java/com/hfut/utils/utils/GsonUtils.kt
@@ -0,0 +1,83 @@
+package com.hfut.base.utils
+
+import android.text.TextUtils
+import com.google.gson.Gson
+import com.google.gson.JsonSyntaxException
+import com.socks.library.KLog
+import java.lang.reflect.Type
+import java.util.*
+
+/**
+ * Created by wzt on 2019/5/16
+ *
+ */
+class GsonUtils{
+    companion object {
+        fun toJson(src: Any?): String {
+            if (src == null) {
+                return ""
+            }
+            val gson = Gson()
+            return gson.toJson(src)
+        }
+
+        fun toJson(src: Any?, typeOfSrc: Type): String {
+            if (src == null) {
+                return ""
+            }
+            val gson = Gson()
+            return gson.toJson(src, typeOfSrc)
+        }
+
+        fun <T> fromJson(json: String?, classOfT: Class<T>): T? {
+            if (TextUtils.isEmpty(json)) {
+                return null
+            }
+            val gson = Gson()
+            return try {
+                gson.fromJson(json, classOfT)
+            } catch (e: JsonSyntaxException) {
+                e.printStackTrace()
+                null
+            } catch (e: NumberFormatException) {
+                e.printStackTrace()
+                null
+            }
+
+        }
+
+        fun <T> fromJson(json: String?, type: Type): T? {
+            if (TextUtils.isEmpty(json)) {
+                return null
+            }
+            val gson = Gson()
+            try {
+                return gson.fromJson<T>(json, type)
+            } catch (e: JsonSyntaxException) {
+                KLog.json(json)
+                e.printStackTrace()
+            }
+
+            return null
+        }
+
+        fun <T> fromJsonToList(json: String?, clazz: Class<Array<T>>): List<T> {
+            if (TextUtils.isEmpty(json)) {
+                return ArrayList()
+            }
+            try {
+                val arr = Gson().fromJson(json, clazz)
+                if (arr == null || arr.isEmpty()) {
+                    return ArrayList()
+                }
+                val ts = Arrays.asList(*arr)
+                return ArrayList(ts)
+            } catch (e: JsonSyntaxException) {
+                KLog.json(json)
+                e.printStackTrace()
+                return ArrayList()
+            }
+
+        }
+    }
+}
