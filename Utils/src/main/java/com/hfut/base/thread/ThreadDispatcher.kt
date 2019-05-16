cc72fc1f39b4f3811f3d1217fcfe9f35cd431f69
wangzitao <417301709@qq.com>
2019-05-16 11:08:51 +0800

    优化线程架构

--- Utils/src/main/java/com/hfut/base/thread/ThreadDispatcher.kt
+++ Utils/src/main/java/com/hfut/base/thread/ThreadDispatcher.kt
@@ -2,7 +2,6 @@ package com.hfut.base.thread
 
 import android.os.Handler
 import android.os.Looper
-import com.socks.library.KLog
 import java.util.concurrent.PriorityBlockingQueue
 import java.util.concurrent.ThreadPoolExecutor
 import java.util.concurrent.TimeUnit
@@ -13,7 +12,7 @@ import java.util.concurrent.TimeUnit
  */
 class ThreadDispatcher private constructor() {
     private val threadPoolExecutor : ThreadPoolExecutor
-    private val waitingQueue : UIBusinessPriorityQueue
+    private val waitingQueue : PriorityBlockingQueue<Runnable> = PriorityBlockingQueue()
     private val mainThreadHandler : Handler
 
     companion object {
@@ -27,7 +26,6 @@ class ThreadDispatcher private constructor() {
     }
 
     init {
-        waitingQueue = UIBusinessPriorityQueue()
         threadPoolExecutor = ThreadPoolExecutor(MAXIMUM_POOL_SIZE, MAXIMUM_POOL_SIZE, KEEP_ALIVE_SECONDS.toLong(), TimeUnit.SECONDS, waitingQueue)
         threadPoolExecutor.allowCoreThreadTimeOut(true)
         mainThreadHandler = Handler(Looper.getMainLooper())
@@ -39,35 +37,68 @@ class ThreadDispatcher private constructor() {
         val instance = ThreadDispatcher()
     }
 
-    inner class UIBusinessPriorityQueue : PriorityBlockingQueue<Runnable>() {
-        override fun poll(timeout: Long, unit: TimeUnit?): Runnable? {
-            val first = peek() as BusinessRunnable?
-            return if (shouldExecute(first)) super.poll(timeout, unit) else null
+    private fun createBusinessRunnable(priority: BusinessPriority, runnable: Runnable): BusinessRunnable {
+        return object : BusinessRunnable(priority) {
+            override fun doWorkInRun() {
+                runnable.run()
+            }
         }
     }
 
-    private fun shouldExecute(runnable: BusinessRunnable?): Boolean {
-        return runnable != null && BusinessPriority.UI === runnable.getPriority() || MAXIMUM_POOL_SIZE - threadPoolExecutor.activeCount > HOLD_THREAD_COUNT
-    }
-
-    fun postToMainThread(run: Runnable?): Boolean {
-        if (run == null) {
-            KLog.e("wzt","ThreadDispatcher : postToMainThread : runnable is null")
-            return false
-        }
+    /**
+     * 提交任务至主线程执行
+     */
+    fun postToMainThread(run: Runnable): Boolean {
         return mainThreadHandler.post(run)
     }
 
-    fun postToMainThreadDelayed(runnable: Runnable?, delay: Long): Boolean {
-        if (runnable == null) {
-            KLog.d("wzt","ThreadDispatcher : postToMainThreadDelayed : runnable is null, delayed " + delay)
-            return false
-        }
+    /**
+     * 提交任务至主线程延迟执行
+     */
+    fun postToMainThreadDelayed(runnable: Runnable, delay: Long): Boolean {
         return mainThreadHandler.postDelayed(runnable, delay)
     }
 
-    fun postToBusinessThread(run: BusinessRunnable): Boolean {
+    /**
+     * 提交任务至子线程执行
+     */
+    fun postToBusinessThread(run: BusinessRunnable) {
         threadPoolExecutor.execute(run)
-        return true
+    }
+
+    /**
+     * 延时提交业务runnable，runnable需设置好对应业务优先级，默认为Normal
+     */
+    fun postBusinessRunnableDelayed(runnable: BusinessRunnable, delay: Long): Boolean {
+        return postToMainThreadDelayed(Runnable { postToBusinessThread(runnable)}, delay)
+    }
+
+    /**
+     * 提交普通业务runnable
+     */
+    fun postNormalBusinessRunnable(runnable: Runnable) {
+        postToBusinessThread(createBusinessRunnable(BusinessPriority.NORMAL, runnable))
+    }
+
+    /**
+     * 提交第三方相关业务runnable
+     */
+    fun postThirdPartyBusinessRunnable(runnable: Runnable) {
+        postToBusinessThread(createBusinessRunnable(BusinessPriority.THIRD_PARTY,runnable))
+    }
+
+    /**
+     * 从队列中移除主线程延时runnable
+     */
+    fun removeRunnableFromMainThread(runnable: Runnable){
+        mainThreadHandler.removeCallbacks(runnable)
+    }
+
+    /**
+     * 从队列中移除相关业务runnable
+     */
+    fun removeBusinessRunnable(runnable: BusinessRunnable) {
+        mainThreadHandler.removeCallbacks(runnable)
+        waitingQueue.remove(runnable)
     }
 }
