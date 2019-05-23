cfd2f1ea646727d6aa466ea4576cfb6948472e69
cmsBWUWB <1078698042@qq.com>
2019-05-23 20:31:49 +0800

    优化Game代码

--- gamelibrary/src/main/java/com/hfut/gamelibrary/Game.java
+++ gamelibrary/src/main/java/com/hfut/gamelibrary/Game.java
@@ -8,34 +8,53 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ *
+ * 游戏的规则不应该定义在这里，因为如果连游戏的规则都不知道，更别说使用Game类了
+ *
  * @author cms
  */
 public class Game {
     //卡牌种类
     public enum Card {
-        DRAGON, DARK, DREAM, CAT_BIRD, FLASH, SNOW, FIRE, BLOOD
+        DRAGON, DARK, DREAM, CAT_BIRD, FLASH, SNOW, FIRE, BLOOD;
+        public static Card indexOf(int index){
+            return Card.class.getEnumConstants()[index];
+        }
     }
-
-    //游戏的下一步是什么？
-    public enum STEP {
-        CURRENT_PLAYER_DO_MAGIC,//当前玩家施放魔法
-        CURRENT_PLAYER_DO_MAGIC_OR_PASS,//请当前玩家选择：继续释放魔法还是结束自己的回合
-        CURRENT_PLAYER_DO_THROW_DICE,//当前玩家掷骰子
-        START_NEW_TURN,//开始新一轮游戏
-        GAME_END//整局游戏结束
-    }
+    //游戏的状态
+    public enum STATUS {ROUND_STARTED, ROUND_ENDED, TURN_ENDED, GAME_ENDED}
 
     public class Player {
-        String userId;
-        int point;//分数
-        int blood;//血量
-        List<Card> cardList;//手里的牌
-        List<Card> secretCard;//猫头鹰的牌
+        private String userId;
+        private int point;//分数
+        private int blood;//血量
+        private List<Card> cardList;//手里的牌
+        private List<Card> secretCard;//猫头鹰的牌
 
-        void changeBlood(int count) {
+        private void changeBlood(int count) {
             blood += count;
             blood = Math.min(6, blood);
             blood = Math.max(0, blood);
+        }
+
+        public int getPoint() {
+            return point;
+        }
+
+        public int getBlood() {
+            return blood;
+        }
+
+        public List<Card> getCardList() {
+            return cardList;
+        }
+
+        public List<Card> getSecretCard() {
+            return secretCard;
+        }
+
+        public String getUserId() {
+            return userId;
         }
 
         @Override
@@ -44,37 +63,31 @@ public class Game {
         }
     }
 
-    /**
-     * 每个玩家的回合都对应着一个Session
-     */
-    class Session {
-        Card whichMagicList;//当前玩家本回合内释放的魔法
-        boolean doMagicSuccess;//当前玩家本回合内释放的魔法是否成功
-        int dice;//当前玩家本回合内魔法掷骰子的点数：0表示没有掷骰子
-    }
-
-
+    //整局游戏相关的信息
     private List<Player> playerList;//所有玩家
-    private int currentTurn;//当前是第几轮游戏？
     private Player gameWinner;//整局游戏的赢家是谁？
+    private STATUS status;
 
 
+    //每轮游戏相关的信息
+    private int[] count;//每组牌的剩余个数
+    private int totalCount;//剩余牌的总牌数
+    private int secretCardCount;//剩余神秘牌组的数量
     private Player turnWinner;//当前轮次的赢家是谁？
     private List<Player> loser;//当前轮次哪些人死了？
 
-    //当前游戏的状态
-    private int[] count;//每组牌的剩余个数
-    private int totalCount;//剩余牌的总牌数
-    private int secretCard;//剩余神秘牌组的数量
-
+    //每个回合相关的信息
     private Player currentPlayer;//现在游戏是谁的回合？？
-    private Session currentSession;
-    private STEP nextStep;//游戏的下一步是什么？
+
+    //每个动作相关的信息
+    private Card whichMagic;
+    private boolean doMagicSuccess;
+    private int dice;
 
     /**
-     * 初始化游戏
+     * 开始游戏
      */
-    public void init(List<User> userList) {
+    public void start(List<User> userList) {
         playerList = new ArrayList<>();
         for (User user : userList) {
             Player player = new Player();
@@ -83,165 +96,61 @@ public class Game {
             player.userId = user.getUserId();
             playerList.add(player);
         }
-        currentTurn = 0;
+        gameWinner = null;
+
+        startTurn();
     }
 
     /**
-     * 获取当前玩家
+     * 开始第一轮游戏
      */
-    public String getCurrentPlayer() {
-        return currentPlayer.userId;
-    }
-
-    public List<String> getPlayList() {
-        List<String> userIdList = new ArrayList<>();
-        for (Player player : playerList) {
-            userIdList.add(player.userId);
+    private void startTurn() {
+        for (Player player: playerList) {
+            player.blood = 6;
         }
-        return userIdList;
+        turnWinner = null;
+        loser = new ArrayList<>();
+
+        postCard();
+
+        startRound();
     }
 
     /**
-     * 获取游戏的下一步是什么？
-     * 1. 当前玩家施放魔法
-     * 2. 当前玩家掷骰子
-     * 3. 开始新一轮游戏
-     * 4. 游戏结束
+     * 开始第一个回合
      */
-    public STEP getNextStep() {
-        return nextStep;
+    private void startRound(){
+        currentPlayer = playerList.get(0);
+
+        whichMagic = null;
+        doMagicSuccess = false;
+        dice = 0;
+
+        status = STATUS.ROUND_STARTED;
     }
 
     /**
-     * 施放魔法
+     * 下一回合
      */
-    public boolean doMagic(Card magic) {
-        currentSession = new Session();
-
-        currentSession.whichMagicList = magic;
-
-        //判断当前玩家是否有该魔法
-        int index = currentPlayer.cardList.indexOf(magic);
-        if (index == -1) {
-            //释放魔法失败
-            currentSession.doMagicSuccess = false;
-            switch (magic) {
-                case DRAGON:
-                    //1. 释放的是巨龙魔法，需要掷骰子，决定自己扣多少血
-                    nextStep = STEP.CURRENT_PLAYER_DO_THROW_DICE;
-                    break;
-                default:
-                    currentPlayer.changeBlood(-1);
-                    if (currentPlayer.blood == 0) {
-                        loser.add(currentPlayer);
-                        gameTurnOver();
-                        return false;
-                    }
-                    //下一个玩家
-                    nextPlayer();
-                    break;
-            }
-            return false;
-        } else {
-            //释放魔法成功
-            currentSession.doMagicSuccess = true;
-            currentPlayer.cardList.remove(index);
-
-            if (currentPlayer.cardList.isEmpty()) {
-                //本轮游戏结束
-                turnWinner = currentPlayer;
-                gameTurnOver();
-                return true;
-            }
-
-            switch (magic) {
-                case DRAGON://古代巨龙
-                case DREAM://甜蜜的梦
-                    //掷骰子
-                    nextStep = STEP.CURRENT_PLAYER_DO_THROW_DICE;
-                    break;
-                case DARK:
-                    //黑暗幽灵
-                    for (Player player : playerList) {
-                        if (!player.equals(currentPlayer))
-                            player.changeBlood(-1);
-                    }
-                    currentPlayer.changeBlood(1);
-                    nextStep = STEP.CURRENT_PLAYER_DO_MAGIC_OR_PASS;
-                    break;
-                case CAT_BIRD:
-                    //猫头鹰
-                    if (secretCard >= 1) {
-                        secretCard--;
-                        currentPlayer.secretCard.add(getCard());
-                    }
-                    nextStep = STEP.CURRENT_PLAYER_DO_MAGIC_OR_PASS;
-                    break;
-                case FLASH:
-                    //闪电暴风雨
-
-                    //右边的玩家扣血
-                    playerList.get((playerList.indexOf(currentPlayer) + 1) % playerList.size()).changeBlood(-1);
-                    //人数只有2人的时候，只会扣一次血
-                    if (playerList.size() != 2) {
-                        //左边的玩家扣血
-                        playerList.get((playerList.indexOf(currentPlayer) - 1 + playerList.size()) % playerList.size()).changeBlood(-1);
-                    }
-                    nextStep = STEP.CURRENT_PLAYER_DO_MAGIC_OR_PASS;
-                    break;
-                case SNOW:
-                    //暴风雪
-                    //左边的玩家扣血
-                    playerList.get((playerList.indexOf(currentPlayer) - 1 + playerList.size()) % playerList.size()).changeBlood(-1);
-                    nextStep = STEP.CURRENT_PLAYER_DO_MAGIC_OR_PASS;
-                    break;
-                case FIRE:
-                    //火球术
-                    //右边的玩家扣血
-                    playerList.get((playerList.indexOf(currentPlayer) + 1) % playerList.size()).changeBlood(-1);
-                    nextStep = STEP.CURRENT_PLAYER_DO_MAGIC_OR_PASS;
-                    break;
-                case BLOOD:
-                    //魔法药水，自己加血
-                    currentPlayer.changeBlood(1);
-                    nextStep = STEP.CURRENT_PLAYER_DO_MAGIC_OR_PASS;
-                    break;
-            }
-            //判断有谁血量为0
-            for (Player player : playerList) {
-                if (player.blood == 0) {
-                    loser.add(player);
-                }
-            }
-            if (!loser.isEmpty()) {
-                turnWinner = currentPlayer;
-                gameTurnOver();
-                return true;
-            }
-            return true;
-        }
-    }
-
-    /**
-     * 下个玩家
-     */
-    private void nextPlayer() {
+    public void nextRound() {
         //补满5张牌
-        while (currentPlayer.cardList.size() < 5 && totalCount > secretCard) {
+        while (currentPlayer.cardList.size() < 5 && totalCount > secretCardCount) {
             currentPlayer.cardList.add(getCard());
         }
-
-        nextStep = STEP.CURRENT_PLAYER_DO_MAGIC;
-
+        whichMagic = null;
+        doMagicSuccess = false;
+        dice = 0;
         int currentPlayerIndex = playerList.indexOf(currentPlayer);
         currentPlayerIndex++;
         currentPlayerIndex %= playerList.size();
         currentPlayer = playerList.get(currentPlayerIndex);
+        status = STATUS.ROUND_STARTED;
     }
 
-    //一轮游戏结束
-    private void gameTurnOver() {
-        nextStep = STEP.START_NEW_TURN;
+    /**
+     * 下一轮游戏
+     */
+    public void nextTurn() {
         if (turnWinner != null) {
             if (!loser.isEmpty()) {
                 //1. 有loser，有winner
@@ -273,63 +182,197 @@ public class Game {
         for (Player player : playerList) {
             if (player.point >= 8) {
                 gameWinner = player;
-                gameOver();
-                break;
+                endGame();
+                return;
             }
         }
+
+        for (Player player: playerList) {
+            player.blood = 6;
+        }
+        turnWinner = null;
+        loser = new ArrayList<>();
+        postCard();
+        startRound();
     }
 
     //整局游戏结束
-    private void gameOver() {
-        nextStep = STEP.GAME_END;
+    private void endGame() {
+        status = STATUS.GAME_ENDED;
     }
 
     /**
-     * 结束当前玩家的回合
+     * 施放魔法
      */
-    public void pass() {
-        nextPlayer();
+    public boolean doMagic(Card magic) {
+        whichMagic = magic;
+        //判断当前玩家是否有该魔法
+        int index = currentPlayer.cardList.indexOf(magic);
+        if (index == -1) {
+            //释放魔法失败
+            doMagicSuccess = false;
+            switch (magic) {
+                case DRAGON:
+                    //释放的是巨龙魔法，需要掷骰子，决定自己扣多少血
+                    break;
+                default:
+                    currentPlayer.changeBlood(-1);
+                    if (currentPlayer.blood == 0) {
+                        loser.add(currentPlayer);
+                        status = STATUS.TURN_ENDED;
+                    }
+                    //回合结束
+                    status = STATUS.ROUND_ENDED;
+                    break;
+            }
+            return false;
+        } else {
+            //释放魔法成功
+            doMagicSuccess = true;
+            currentPlayer.cardList.remove(index);
+
+            if (currentPlayer.cardList.isEmpty()) {
+                //本轮游戏结束
+                turnWinner = currentPlayer;
+                status = STATUS.TURN_ENDED;
+                return true;
+            }
+
+            switch (magic) {
+                case DRAGON://古代巨龙
+                case DREAM://甜蜜的梦
+                    //掷骰子
+                    break;
+                case DARK:
+                    //黑暗幽灵
+                    for (Player player : playerList) {
+                        if (!player.equals(currentPlayer))
+                            player.changeBlood(-1);
+                    }
+                    currentPlayer.changeBlood(1);
+                    break;
+                case CAT_BIRD:
+                    //猫头鹰
+                    if (secretCardCount >= 1) {
+                        secretCardCount--;
+                        currentPlayer.secretCard.add(getCard());
+                    }
+                    break;
+                case FLASH:
+                    //闪电暴风雨
+
+                    //右边的玩家扣血
+                    playerList.get((playerList.indexOf(currentPlayer) + 1) % playerList.size()).changeBlood(-1);
+                    //人数只有2人的时候，只会扣一次血
+                    if (playerList.size() != 2) {
+                        //左边的玩家扣血
+                        playerList.get((playerList.indexOf(currentPlayer) - 1 + playerList.size()) % playerList.size()).changeBlood(-1);
+                    }
+                    break;
+                case SNOW:
+                    //暴风雪
+                    //左边的玩家扣血
+                    playerList.get((playerList.indexOf(currentPlayer) - 1 + playerList.size()) % playerList.size()).changeBlood(-1);
+                    break;
+                case FIRE:
+                    //火球术
+                    //右边的玩家扣血
+                    playerList.get((playerList.indexOf(currentPlayer) + 1) % playerList.size()).changeBlood(-1);
+                    break;
+                case BLOOD:
+                    //魔法药水，自己加血
+                    currentPlayer.changeBlood(1);
+                    break;
+            }
+            //判断有谁血量为0
+            for (Player player : playerList) {
+                if (player.blood == 0) {
+                    loser.add(player);
+                }
+            }
+            if (!loser.isEmpty()) {
+                turnWinner = currentPlayer;
+                status = STATUS.TURN_ENDED;
+            }
+            return true;
+        }
     }
 
     /**
      * 掷骰子
      */
     public int doThrowDice() {
-        currentSession.dice = getDice();
+        dice = (int) (Math.random() * 3) + 1;
 
-        switch (currentSession.whichMagicList) {
+        switch (whichMagic) {
             case DRAGON:
-                if (currentSession.doMagicSuccess) {
+                if (doMagicSuccess) {
                     for (Player player : playerList) {
                         if (!player.equals(currentPlayer))
-                            player.changeBlood(-1 * currentSession.dice);
+                            player.changeBlood(-1 * dice);
                     }
-                    nextStep = STEP.CURRENT_PLAYER_DO_MAGIC_OR_PASS;
                 } else {
-                    currentPlayer.changeBlood(-1 * currentSession.dice);
+                    currentPlayer.changeBlood(-1 * dice);
                     if (currentPlayer.blood == 0) {
                         loser.add(currentPlayer);
-                        gameTurnOver();
-                        return currentSession.dice;
+                        status = STATUS.TURN_ENDED;
+                        return dice;
                     }
-                    nextPlayer();
+                    status = STATUS.ROUND_ENDED;
                 }
                 break;
             case DREAM:
-                currentPlayer.changeBlood(currentSession.dice);
-                nextStep = STEP.CURRENT_PLAYER_DO_MAGIC_OR_PASS;
+                currentPlayer.changeBlood(dice);
                 break;
         }
-        return currentSession.dice;
+        return dice;
     }
 
     /**
-     * 开始新一轮游戏
+     * 玩家主动结束自己的回合
      */
-    public void startNewTurn() {
+    public void pass() {
+        status = STATUS.ROUND_ENDED;
+    }
+
+    /**
+     * 获取所有玩家
+     */
+    public List<Player> getPlayerList(){
+        return playerList;
+    }
+
+    /**
+     * 还剩什么牌？
+     */
+    public int[] getCount(){
+        return count;
+    }
+
+    /**
+     * 神秘牌组还剩多少张牌？
+     */
+    public int getSecretCardCount(){
+        return secretCardCount;
+    }
+    public Player getGameWinner(){
+        return gameWinner;
+    }
+    public Player getTurnWinner(){
+        return turnWinner;
+    }
+    public STATUS getStatus(){
+        return status;
+    }
+
+    /**
+     * 发牌
+     */
+    private void postCard(){
+        //发牌
         count = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
         totalCount = 36;
-
+        secretCardCount = 4;
         //两个人就移除12张牌，三个人就移除6张牌
         if (playerList.size() == 2) {
             //移除12个
@@ -348,16 +391,6 @@ public class Game {
             }
             player.secretCard = new ArrayList<>();
         }
-
-        currentTurn++;
-        currentPlayer = playerList.get(0);
-        currentSession = new Session();
-        loser = new ArrayList<>();
-        nextStep = STEP.CURRENT_PLAYER_DO_MAGIC;
-    }
-
-    private int getDice() {
-        return (int) (Math.random() * 3) + 1;
     }
 
     /**
@@ -369,7 +402,7 @@ public class Game {
             if ((random -= count[i]) <= 0) {
                 count[i]--;
                 totalCount--;
-                return Card.class.getEnumConstants()[i];
+                return Card.indexOf(i);
             }
         }
         //理论上永远不会执行到这一步
