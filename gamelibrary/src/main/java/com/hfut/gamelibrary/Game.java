ed27e9fad9af03b06d18f917c74259ffe6e7387c
cmsBWUWB <1078698042@qq.com>
2019-05-23 14:53:18 +0800

    完成Game所有功能

--- gamelibrary/src/main/java/com/hfut/gamelibrary/Game.java
+++ gamelibrary/src/main/java/com/hfut/gamelibrary/Game.java
@@ -25,15 +25,14 @@ public class Game {
         GAME_END//整局游戏结束
     }
 
-    private STEP nextStep;
-
     public class Player {
         String userId;
         int point;//分数
         int blood;//血量
         List<Card> cardList;//手里的牌
+        List<Card> secretCard;//猫头鹰的牌
 
-        public void changeBlood(int count) {
+        void changeBlood(int count) {
             blood += count;
             blood = Math.min(6, blood);
             blood = Math.max(0, blood);
@@ -45,25 +44,33 @@ public class Game {
         }
     }
 
+    /**
+     * 每个玩家的回合都对应着一个Session
+     */
+    class Session {
+        Card whichMagicList;//当前玩家本回合内释放的魔法
+        boolean doMagicSuccess;//当前玩家本回合内释放的魔法是否成功
+        int dice;//当前玩家本回合内魔法掷骰子的点数：0表示没有掷骰子
+    }
+
+
     private List<Player> playerList;//所有玩家
+    private int currentTurn;//当前是第几轮游戏？
+    private Player gameWinner;//整局游戏的赢家是谁？
+
+
+    private Player turnWinner;//当前轮次的赢家是谁？
+    private List<Player> loser;//当前轮次哪些人死了？
 
     //当前游戏的状态
     private int[] count;//每组牌的剩余个数
     private int totalCount;//剩余牌的总牌数
+    private int secretCard;//剩余神秘牌组的数量
 
-    private int currentTurn;//当前是第几轮游戏？
     private Player currentPlayer;//现在游戏是谁的回合？？
-
-    /**
-     * 每个玩家的回合都对应着一个Session
-     */
-    public class Session {
-        public Card whichMagicList;//当前玩家本回合内释放的魔法
-        public boolean doMagicSuccess;//当前玩家本回合内释放的魔法是否成功
-        public int dice;//当前玩家本回合内魔法掷骰子的点数：-1表示没有掷骰子
-    }
-
     private Session currentSession;
+    private Session lastSession;
+    private STEP nextStep;
 
     /**
      * 初始化游戏
@@ -102,12 +109,10 @@ public class Game {
      * 施放魔法
      */
     public void doMagic(Card magic) {
-        currentSession = new Session();
         currentSession.whichMagicList = magic;
 
         //判断当前玩家是否有该魔法
         int index = currentPlayer.cardList.indexOf(magic);
-
         if (index == -1) {
             //释放魔法失败
             currentSession.doMagicSuccess = false;
@@ -117,9 +122,14 @@ public class Game {
                     nextStep = STEP.CURRENT_PLAYER_DO_THROW_DICE;
                     break;
                 default:
+                    currentPlayer.changeBlood(-1);
+                    if(currentPlayer.blood == 0){
+                        loser.add(currentPlayer);
+                        gameTurnOver();
+                        return;
+                    }
                     //下一个玩家
                     nextPlayer();
-                    nextStep = STEP.CURRENT_PLAYER_DO_MAGIC;
                     break;
             }
         } else {
@@ -127,9 +137,16 @@ public class Game {
             currentSession.doMagicSuccess = true;
             currentPlayer.cardList.remove(index);
 
+            if (currentPlayer.cardList.isEmpty()) {
+                //本轮游戏结束
+                turnWinner = currentPlayer;
+                gameTurnOver();
+                return;
+            }
+
             switch (magic) {
-                case DRAGON:
-                case DREAM:
+                case DRAGON://古代巨龙
+                case DREAM://甜蜜的梦
                     //掷骰子
                     nextStep = STEP.CURRENT_PLAYER_DO_THROW_DICE;
                     break;
@@ -139,26 +156,101 @@ public class Game {
                         if (!player.equals(currentPlayer))
                             player.changeBlood(-1);
                     }
+                    currentPlayer.changeBlood(1);
                     nextStep = STEP.CURRENT_PLAYER_DO_MAGIC_OR_PASS;
                     break;
+                case CAT_BIRD:
+                    //猫头鹰
+                    if (secretCard >= 1) {
+                        secretCard--;
+                        currentPlayer.secretCard.add(getCard());
+                    }
+                    nextStep = STEP.CURRENT_PLAYER_DO_MAGIC_OR_PASS;
+                    break;
+                case FLASH:
+                    //闪电暴风雨
+
+                    //右边的玩家扣血
+                    playerList.get((playerList.indexOf(currentPlayer) + 1) % playerList.size()).changeBlood(-1);
+                    //人数只有2人的时候，只会扣一次血
+                    if (playerList.size() != 2) {
+                        //左边的玩家扣血
+                        playerList.get((playerList.indexOf(currentPlayer) - 1 + playerList.size()) % playerList.size()).changeBlood(-1);
+                    }
+                    nextStep = STEP.CURRENT_PLAYER_DO_MAGIC_OR_PASS;
+                    break;
+                case SNOW:
+                    //暴风雪
+                    //左边的玩家扣血
+                    playerList.get((playerList.indexOf(currentPlayer) - 1 + playerList.size()) % playerList.size()).changeBlood(-1);
+                    nextStep = STEP.CURRENT_PLAYER_DO_MAGIC_OR_PASS;
+                    break;
+                case FIRE:
+                    //火球术
+                    //右边的玩家扣血
+                    playerList.get((playerList.indexOf(currentPlayer) + 1) % playerList.size()).changeBlood(-1);
+                    nextStep = STEP.CURRENT_PLAYER_DO_MAGIC_OR_PASS;
+                    break;
+                case BLOOD:
+                    //魔法药水，自己加血
+                    currentPlayer.changeBlood(1);
+                    nextStep = STEP.CURRENT_PLAYER_DO_MAGIC_OR_PASS;
+                    break;
+            }
+            //判断有谁血量为0
+            for(Player player:playerList){
+                if(player.blood == 0){
+                    loser.add(player);
+                }
+            }
+            if(!loser.isEmpty()){
+                turnWinner = currentPlayer;
+                gameTurnOver();
+                return;
             }
         }
     }
 
+    /**
+     * 下个玩家
+     */
     private void nextPlayer() {
-        currentSession = null;
+        //补满5张牌
+        while (currentPlayer.cardList.size() < 5 && totalCount > secretCard) {
+            currentPlayer.cardList.add(getCard());
+        }
+
+        nextStep = STEP.CURRENT_PLAYER_DO_MAGIC;
 
         int currentPlayerIndex = playerList.indexOf(currentPlayer);
         currentPlayerIndex++;
         currentPlayerIndex %= playerList.size();
         currentPlayer = playerList.get(currentPlayerIndex);
+        lastSession = currentSession;
+        currentSession = new Session();
+    }
+
+    //一轮游戏结束
+    private void gameTurnOver() {
+        nextStep = STEP.START_NEW_TURN;
+        for (Player player : playerList) {
+            if (player.point >= 8) {
+                gameWinner = player;
+                gameOver();
+                break;
+            }
+        }
+    }
+
+    //整局游戏结束
+    private void gameOver() {
+        nextStep = STEP.GAME_END;
     }
 
     /**
      * 结束当前玩家的回合
      */
     public void pass() {
-        nextStep = STEP.CURRENT_PLAYER_DO_MAGIC;
         nextPlayer();
     }
 
@@ -178,13 +270,17 @@ public class Game {
                     nextStep = STEP.CURRENT_PLAYER_DO_MAGIC_OR_PASS;
                 } else {
                     currentPlayer.changeBlood(-1 * currentSession.dice);
-
+                    if(currentPlayer.blood == 0){
+                        loser.add(currentPlayer);
+                        gameTurnOver();
+                        return;
+                    }
                     nextPlayer();
-                    nextStep = STEP.CURRENT_PLAYER_DO_MAGIC;
                 }
                 break;
             case DREAM:
                 currentPlayer.changeBlood(currentSession.dice);
+                nextStep = STEP.CURRENT_PLAYER_DO_MAGIC_OR_PASS;
                 break;
         }
     }
@@ -212,11 +308,13 @@ public class Game {
             for (int i = 0; i < 5; i++) {
                 player.cardList.add(getCard());
             }
+            player.secretCard = new ArrayList<>();
         }
 
         currentTurn++;
         currentPlayer = playerList.get(0);
-        currentSession = null;
+        currentSession = new Session();
+        loser = new ArrayList<>();
         nextStep = STEP.CURRENT_PLAYER_DO_MAGIC;
     }
 
